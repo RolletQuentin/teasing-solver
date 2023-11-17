e1a6d0b742005e4d1e1fe83fdbc99f3489192a72
RolletQuentin <rollet.quentin@orange.fr>
2023-11-17 15:31:31 +0100

    start graphic interface

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-if __name__ == "__main__":
-    print("Hello world !")
+from fastapi import FastAPI, HTTPException
+
+from src.Teasing import Teasing
+
+app = FastAPI()
+
+game = None
+
+
+@app.get("/start_game/")
+async def start_game():
+    global game
+    game = Teasing()
+    return {"message": "Game started!", "board": game.board.tolist()}
+
+
+@app.post("/make_move/")
+async def make_move(pos: tuple, direction: str):
+    global game
+    if game is None:
+        raise HTTPException(
+            status_code=400, detail="Game not started. Please start a game first.")
+
+    pos = tuple(pos)
+    try:
+        game.move(pos, direction)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    if game.win():
+        return {"message": "You won!", "board": game.board.tolist()}
+    else:
+        return {"message": "Move successful", "board": game.board.tolist}
