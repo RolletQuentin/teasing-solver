39df7b355b6de848c53c48442437b0d240c47694
RolletQuentin <rollet.quentin@orange.fr>
2023-11-19 10:24:03 +0100

    change the way to shuffle the game
    we the previous method, the random board had 1/2 chance to be unsolved. Now, we start with the final solution and we make regular random moves to shuffle

--- src/Teasing.py
+++ src/Teasing.py
@@ -17,13 +17,24 @@ class Teasing:
 
     """
 
-    def __init__(self, seed=-1):
+    def __init__(self, x=3, y=3, seed=-1):
         """Constructs all the necessary attributes for the person object.
 
         Args:
             seed (str, optional): The seed for how the teasing game is set. -1 is for a random seed. Defaults to -1.
         """
-        self.board = self.random_teasing_board(seed)
+        self.x = x
+        self.y = y
+        self.seed = seed
+
+        # generate the solution
+        solution = np.arange(1, self.x*self.y)
+        solution = np.append(solution, 0)
+        self.solution = solution.reshape((self.x, self.y))
+
+        # generate random board
+        self.board = self.solution.copy()
+        self.random_teasing_board()
 
     def __str__(self):
         horizontal_line = "-------------\n"
@@ -39,8 +50,8 @@ class Teasing:
 
         return result
 
-    def random_teasing_board(self, seed=-1):
-        """Generate a random board for the teasing game. All numbers from 0 to 8 will appear once on the board, and the shape of the board is (3,3). If the seed is -1, a random seed will be used.
+    def random_teasing_board(self):
+        """Generate a random board for the teasing game. If the seed is -1, a random seed will be used.
 
         Args:
             seed (int, optional): Seed for random number generation. If not provided or set to -1, a random seed will be used.
@@ -48,21 +59,18 @@ class Teasing:
         Returns:
             numpy.ndarray: A 2D NumPy array representing the random teasing board.
         """
-        if seed == -1:
+        if self.seed == -1:
             np.random.seed()
         else:
-            np.random.seed(seed)
+            np.random.seed(self.seed)
 
-        numbers = np.arange(9)
-        np.random.shuffle(numbers)
-
-        teasing_board = numbers.reshape((3, 3))
-        return teasing_board
+        for _ in range(1000):
+            self.move((np.random.randint(self.x), np.random.randint(self.y)))
 
     def win(self):
         """Return True if the game is finished, else False
         """
-        return np.array_equal(self.board, np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]]))
+        return np.array_equal(self.board, self.solution)
 
     def move(self, pos):
         """Move one box
